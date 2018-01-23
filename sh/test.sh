bb05e3c764580c34a76357841319abfba5109781
khai96_ <hvksmr1996@gmail.com>
2018-01-23 17:11:52 +0700

    Use Jest to test

--- sh/test.sh
+++ sh/test.sh
@@ -1,11 +1,1 @@
-(
-  printf "Checking Code Style... "
-  standard > stdout.tmp 2> stderr.tmp && (
-    echo "passed"
-  ) || (
-    echo "failed" >&2
-    cat stderr.tmp >&2
-    cat stdout.tmp
-    exit 2
-  )
-)
+jest --coverage $@
