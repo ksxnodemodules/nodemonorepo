d92e630bc56d600fca5ea209fd057b91c86d8793
khai96_ <hvksmr1996@gmail.com>
2018-04-07 16:12:40 +0700

    Global tsconfig.json is also needed
    For ts-jest to work

--- sh/build.sh
+++ sh/build.sh
@@ -5,6 +5,7 @@ if [[ $SKIP_TYPESCRIPT_COMPILATION == 'true' ]]
     echo 'Skip TypeScript compilation'
   else(
     echo 'Running TypeScript compiler...'
+    cp 'tsconfig.json' 'packages/typescript'
     cd 'packages/typescript'
     tsc
   )
