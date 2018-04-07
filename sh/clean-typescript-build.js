9cda82fc2a45da6f3e75b0e3519f64e5b0aead5d
khai96_ <hvksmr1996@gmail.com>
2018-04-07 23:41:57 +0700

    Don't ignore .inrepo

--- sh/clean-typescript-build.js
+++ sh/clean-typescript-build.js
@@ -10,6 +10,7 @@ const files = glob.sync('packages/typescript/**/*.{js,d.ts,js.map}')
 
 for (const x of files) {
   if (/node_modules/.test(x)) continue
+  if (/\.inrepo/.test(x)) continue
   console.info('  --> Deleting', x)
   fs.unlink(x, error => error && console.error(`Failed to delete ${x}`, {error}))
 }
