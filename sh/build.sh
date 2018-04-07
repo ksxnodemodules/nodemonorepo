87305a97ae6cd86d039d0bb9eb58732f6ca113bf
khai96_ <hvksmr1996@gmail.com>
2018-04-07 16:02:59 +0700

    Compile typescript in packages/typescript only

--- sh/build.sh
+++ sh/build.sh
@@ -3,7 +3,9 @@ preloaded-node 'sh/clean-typescript-build'
 if [[ $SKIP_TYPESCRIPT_COMPILATION == 'true' ]]
   then
     echo 'Skip TypeScript compilation'
-  else
+  else(
     echo 'Running TypeScript compiler...'
+    cd 'packages/typescript'
     tsc
+  )
 fi
