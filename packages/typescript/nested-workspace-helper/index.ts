20f14a2941f7bc2916a8939c4bb389809542df41
khai96_ <hvksmr1996@gmail.com>
2018-04-12 11:41:51 +0700

    Forgot to add it to index

--- packages/typescript/nested-workspace-helper/index.ts
+++ packages/typescript/nested-workspace-helper/index.ts
@@ -2,4 +2,4 @@ import listAllPackages from './lib/list-pkgs'
 import getDependencyMap from './lib/dep-map'
 import listMismatchedDependencies from './lib/mismatches'
 export * from './lib/types'
-export {listAllPackages, getDependencyMap}
+export {listAllPackages, getDependencyMap, listMismatchedDependencies}
