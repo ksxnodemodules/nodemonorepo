f58dc88974e5179002c8175f6b9190e1b32f8242
khai96_ <hvksmr1996@gmail.com>
2018-04-13 20:43:58 +0700

    Create updateDependencyVersions

--- packages/typescript/nested-workspace-helper/index.ts
+++ packages/typescript/nested-workspace-helper/index.ts
@@ -1,5 +1,13 @@
 import listAllPackages from './lib/list-pkgs'
 import getDependencyMap from './lib/dep-map'
 import listMismatchedDependencies from './lib/mismatches'
+import updateDependencyVersions from './lib/update-dep-vers'
+
 export * from './lib/types'
-export {listAllPackages, getDependencyMap, listMismatchedDependencies}
+
+export {
+  listAllPackages,
+  getDependencyMap,
+  listMismatchedDependencies,
+  updateDependencyVersions
+}
---
+++ packages/typescript/nested-workspace-helper/lib/update-dep-vers.ts
@@ -0,0 +1,94 @@
+import * as fsx from 'fs-extra'
+import getMismatchedDependencies, {Checker} from './mismatches'
+
+import {
+  PackageList,
+  PackageListItem,
+  PackageDict,
+  PackageManifest,
+  MismatchedDependencyMap,
+  MismatchedDependencyList
+} from './types'
+
+export async function updateDependencyVersions (
+  dirname: string,
+  check: Checker,
+  jsonIndent: number | string = 2
+) {
+  await Promise.all(
+    updateDependencyVersions.fromMismatchedDependencyMap(
+      await getMismatchedDependencies(dirname, check)
+    ).map(item => fsx.writeFile(
+      item.manifestFile,
+      JSON.stringify(item.manifestContent, undefined, jsonIndent)
+    ))
+  )
+}
+
+export namespace updateDependencyVersions {
+  export function fromPackageList (pkgs: PackageList, check: Checker) {
+    return fromMismatchedDependencyMap(
+      getMismatchedDependencies.fromPackageList(pkgs, check)
+    )
+  }
+
+  export function fromMismatchedDependencyMap (map: MismatchedDependencyMap) {
+    const result: PackageList = []
+
+    for (const {dependant, list} of Object.values(map)) {
+      result.push({
+        ...dependant,
+        manifestContent: updateDependencies(
+          dependant.manifestContent,
+          list
+        )
+      })
+    }
+
+    return result
+  }
+
+  export function updateDependencies (
+    manifest: PackageManifest,
+    list: MismatchedDependencyList
+  ): PackageManifest {
+    const refs: {
+      [_: string]: PackageDict
+    } = {
+      prod: {},
+      dev: {},
+      peer: {}
+    }
+
+    for (const {type, name, update} of list) {
+      refs[type][name] = update
+    }
+
+    const result = {...manifest}
+
+    const getDict = (
+      prev: PackageDict | void,
+      current: PackageDict
+    ): PackageDict | void => {
+      if (prev) return {...prev, ...current}
+      if (Object.keys(current).length) return current
+      return undefined
+    }
+
+    const setDict = (
+      name: 'dependencies' | 'devDependencies' | 'peerDependencies',
+      dict: PackageDict
+    ): void => {
+      const newDict = getDict(manifest[name] as PackageDict, dict)
+      if (newDict) manifest[name] = newDict
+    }
+
+    setDict('dependencies', refs.prod)
+    setDict('devDependencies', refs.dev)
+    setDict('peerDependencies', refs.peer)
+
+    return result
+  }
+}
+
+export default updateDependencyVersions
