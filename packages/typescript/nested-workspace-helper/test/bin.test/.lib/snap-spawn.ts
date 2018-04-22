daca0e08bbd59ff1a365aff4f1fd17d113adb8e5
khai96_ <hvksmr1996@gmail.com>
2018-04-22 13:37:12 +0700

    Create snapSpawn.promise

--- packages/typescript/nested-workspace-helper/test/bin.test/.lib/snap-spawn.ts
+++ packages/typescript/nested-workspace-helper/test/bin.test/.lib/snap-spawn.ts
@@ -1,4 +1,5 @@
 import {SpawnSyncOptions} from 'child_process'
+import ramda from 'ramda'
 import * as xjest from 'extra-jest'
 import spawn from './spawn'
 
@@ -6,7 +7,13 @@ export {spawn}
 
 export const snap = (x: any) => xjest.snap.unsafe(x)()
 
-export const snapSpawn = (argv?: string[], options?: SpawnSyncOptions) =>
-  () => snap(spawn(argv, options))
+export function snapSpawn (argv?: string[], options?: SpawnSyncOptions) {
+  return () => snap(spawn(argv, options))
+}
+
+export namespace snapSpawn {
+  export const promise = (argv?: string[], options?: SpawnSyncOptions) =>
+    ramda.pipe(snapSpawn(argv, options), async <X>(x: X) => x)
+}
 
 export default snapSpawn
