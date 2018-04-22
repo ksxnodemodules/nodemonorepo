1bd0d6167187dfd89623021ae651602f56a38a00
khai96_ <hvksmr1996@gmail.com>
2018-04-22 13:26:30 +0700

    Use custom register

--- packages/javascript/preloaded-node/bin.js
+++ packages/javascript/preloaded-node/bin.js
@@ -3,7 +3,8 @@ const path = require('path')
 const {argv, env, exit} = require('process')
 const {spawnSync} = require('child_process')
 const receivedArgv = argv.slice(2)
-const preloadedArgv = ['--require', 'ts-node/register/type-check']
+const register = require.resolve('./register/es2017')
+const preloadedArgv = ['--require', register]
 const finalArgv = [...preloadedArgv, ...receivedArgv]
 const {PATH = '', NODE_PATH = ''} = env
 
---
+++ packages/javascript/preloaded-node/register/es2017.js
@@ -0,0 +1,9 @@
+require('ts-node').register({
+  typeCheck: true,
+  compilerOptions: {
+    target: 'es2017',
+    module: 'commonjs',
+    esModuleInterop: true,
+    strict: true
+  }
+})
