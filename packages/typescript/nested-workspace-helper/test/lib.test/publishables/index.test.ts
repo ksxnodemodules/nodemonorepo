57db88a0a4c136bf303fee6ba1986f1e3493bf30
khai96_ <hvksmr1996@gmail.com>
2018-04-26 12:24:46 +0700

    Complete test for publishables

--- packages/typescript/nested-workspace-helper/test/lib.test/publishables/index.test.ts
+++ packages/typescript/nested-workspace-helper/test/lib.test/publishables/index.test.ts
@@ -1,4 +1,18 @@
-import partition from '../../../lib/publishables'
 import createSetupTeardown from '../../.lib/setup-teardown'
+import partition from '../../../lib/publishables'
 
-it('ok', () => {})
+import {
+  PublishablePackageList,
+  UnpublisablePackageList,
+  PrivatePackageList,
+  PublishableClassification
+} from '../../../lib/types'
+
+type PackageList = PublishablePackageList | UnpublisablePackageList | PrivatePackageList
+
+const {apply} = createSetupTeardown('publishability.yaml')
+
+it('matches snapshot', apply(async () => {
+  const classified = await partition('root')
+  expect(classified).toMatchSnapshot()
+}))
