b331ea77d6fb6a8749ca6af8ef68ce4585a8205a
khai96_ <hvksmr1996@gmail.com>
2018-04-28 07:55:59 +0700

    Add more assertions

--- packages/typescript/nested-workspace-helper/test/lib.test/invalids/index.test.ts
+++ packages/typescript/nested-workspace-helper/test/lib.test/invalids/index.test.ts
@@ -1,4 +1,5 @@
 import createSetupTeardown from '../../.lib/setup-teardown'
+import listAllPackages from '../../../lib/list-pkgs'
 import listAllInvalidPackages from '../../../lib/invalids'
 
 const {apply} = createSetupTeardown('invalids.yaml')
@@ -19,3 +20,21 @@ it('contains only invalid packages', apply(async () => {
   const failure = result.filter(x => x.manifestContent.__expect !== 'Invalid')
   expect(failure).toEqual([])
 }))
+
+it('leaves only valid packages', apply(async () => {
+  const failure = (await getComplementPromise()).filter(x => x.manifestContent.__expect !== 'Valid')
+  expect(failure).toEqual([])
+}))
+
+it('leaves no invalid packages', apply(async () => {
+  const failure = (await getComplementPromise()).filter(x => x.manifestContent.__expect === 'Invalid')
+  expect(failure).toEqual([])
+}))
+
+async function getComplementPromise () {
+  const list = await listAllPackages('root')
+  const invalids = await listAllInvalidPackages('root')
+  const invalidPaths = invalids.map(x => x.path)
+  const valids = list.filter(x => !invalidPaths.includes(x.path))
+  return valids
+}
