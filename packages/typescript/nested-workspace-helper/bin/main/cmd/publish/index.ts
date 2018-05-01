e09119153728c15c686b280d7eb6b2b672ed2123
khai96_ <hvksmr1996@gmail.com>
2018-05-01 17:28:03 +0700

    Create command publish

--- packages/typescript/nested-workspace-helper/bin/main/cmd/publish/index.ts
+++ packages/typescript/nested-workspace-helper/bin/main/cmd/publish/index.ts
@@ -1,3 +1,14 @@
+import * as process from 'process'
+import {spawnSync} from 'child_process'
+import chalk from 'chalk'
+import npmRegistry from '../../../../lib/npm-registry'
+import listAllPackages from '../../../../lib/list-pkgs'
+import getDependencyMap from '../../../../lib/dep-map'
+import checkPublisability from '../../../../lib/publishables'
+import listAllInvalidPackages from '../../../../lib/invalids'
+import {getString as ivls2text} from '../../lib/ivls-cvts'
+import {getString as pkgs2text} from '../../lib/list-cvts'
+
 import {
   Argv,
   Arguments,
@@ -5,8 +16,39 @@ import {
   CommandModule
 } from 'yargs'
 
-import checkPublishability from '../../../../lib/npm-registry'
-import listAllInvalidPackages from '../../../../lib/list-pkgs'
+enum ExitStatus {
+  Success = 0,
+  Failure = 1,
+  InvalidPackages = 2,
+  UnpublishablePackages = 3
+}
+
+namespace publisher {
+  export function real (cmd: string, cwd: string): Result {
+    const {status, error, signal} = spawnSync(
+      cmd,
+      ['publish'],
+      {
+        cwd,
+        stdio: 'inherit',
+        shell: true
+      }
+    )
+
+    return {status, error, signal}
+  }
+
+  export function fake (cmd: string, cwd: string): Result {
+    console.info(`Will execute "${cmd} publish" at directory ${cwd}`)
+    return {status: 0, error: undefined, signal: undefined}
+  }
+
+  export interface Result {
+    readonly status: number
+    readonly error?: Error
+    readonly signal?: string
+  }
+}
 
 function builder (yargs: Argv): Argv {
   return yargs
@@ -21,10 +63,21 @@ function builder (yargs: Argv): Argv {
         type: 'boolean',
         default: false
       },
+      executable: {
+        alias: ['publisher', 'command', 'cmd', 'npm'],
+        describe: 'Name of executable that will be called using syntax "<executable> publish"',
+        type: 'string',
+        default: 'npm'
+      },
       registry: {
         describe: 'Registry for checking publishability and publishing packages',
         type: 'string',
-        default: checkPublishability.REGISTRIES.NPM
+        default: npmRegistry.REGISTRIES.NPM
+      },
+      showExitStatus: {
+        describe: 'Show meaning of exit status codes',
+        type: 'boolean',
+        default: false
       }
     })
 }
@@ -32,13 +85,61 @@ function builder (yargs: Argv): Argv {
 function handler ({
   directory,
   dry,
-  registry
+  executable,
+  registry,
+  showExitStatus
 }: Arguments & {
   directory: string,
   dry: boolean,
-  registry: string
+  executable: string,
+  registry: string,
+  showExitStatus: boolean
 }) {
+  main().then(
+    code => process.exit(code),
+    error => {
+      console.error(error)
+      process.exit(ExitStatus.Failure)
+    }
+  )
 
+  async function main (): Promise<number> {
+    if (showExitStatus) {
+      console.info(ExitStatus)
+      return ExitStatus.Success
+    }
+
+    const allPackages = await listAllPackages(directory)
+    const depMap = getDependencyMap.fromPackageList(allPackages)
+    const invalids = listAllInvalidPackages.fromDependencyMap(depMap)
+    const publishability = await checkPublisability.fromPackageList(allPackages)
+
+    if (invalids.length) {
+      console.error('[ERROR] Invalid packages')
+      console.info(ivls2text(invalids))
+      return ExitStatus.InvalidPackages
+    }
+
+    if (publishability.unpublishables.length) {
+      console.error('[ERROR] Unpublishable packages: Version already exist in registry')
+      console.info(pkgs2text(publishability.unpublishables))
+      return ExitStatus.UnpublishablePackages
+    }
+
+    const publish = dry ? publisher.fake : publisher.real
+
+    let finalStatus = 0
+
+    for (const item of publishability.publishables) {
+      const {name} = item.manifestContent
+      console.info(`Publishing ${chalk.bold(name as string)}...`)
+      const {status, error, signal} = publish(executable, item.path)
+      if (status || error || signal) console.info('[FAILED]', {status, error, signal})
+      finalStatus |= status
+    }
+
+    return finalStatus
+  }
 }
 
 export default {
