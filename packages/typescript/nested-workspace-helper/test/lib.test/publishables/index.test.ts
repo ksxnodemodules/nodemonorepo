238ef0b5dbb96df570fbbbc6878b0827a6bd611c
khai96_ <hvksmr1996@gmail.com>
2018-05-01 18:27:52 +0700

    Create tests to track aforementioned bug

--- packages/typescript/nested-workspace-helper/test/lib.test/publishables/index.test.ts
+++ packages/typescript/nested-workspace-helper/test/lib.test/publishables/index.test.ts
@@ -12,7 +12,24 @@ type PackageList = PublishablePackageList | UnpublisablePackageList | PrivatePac
 
 const {apply} = createSetupTeardown('publishability.yaml')
 
-it('matches snapshot', apply(async () => {
-  const classified = await classifyPublishability('root')
-  expect(classified).toMatchSnapshot()
-}))
+describe('matches snapshot', () => {
+  it('when there are all 3 types', apply(async () => {
+    const classified = await classifyPublishability('root')
+    expect(classified).toMatchSnapshot()
+  }))
+
+  it('when there is only publishable packages', apply(async () => {
+    const classified = await classifyPublishability('root/toBePublishable')
+    expect(classified).toMatchSnapshot()
+  }))
+
+  it('when there is only unpublishable packages', apply(async () => {
+    const classified = await classifyPublishability('root/toBeUnpublishable')
+    expect(classified).toMatchSnapshot()
+  }))
+
+  it('when there is only skipped packages', apply(async () => {
+    const classified = await classifyPublishability('root/toBeSkipped')
+    expect(classified).toMatchSnapshot()
+  }))
+})
