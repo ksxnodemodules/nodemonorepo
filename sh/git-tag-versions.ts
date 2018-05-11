301b9adc9198b9cbf45e77593ec7d964e9791033
khai96_ <hvksmr1996@gmail.com>
2018-05-11 15:21:01 +0700

    Create git-tag-versions

---
+++ sh/git-tag-versions.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env preloaded-node
+import * as path from 'path'
+import * as process from 'process'
+import * as childProcess from 'child_process'
+import * as wrkspc from 'nested-workspace-helper.private'
+
+main().then(
+  code => process.exit(code),
+  error => {
+    console.error(error)
+    process.exit(1)
+  }
+)
+
+async function main (): Promise<number> {
+  const projdir = path.resolve(__dirname, '..')
+  const {publishables} = await wrkspc.classifyPublishability(projdir)
+
+  let finalStatus = 0
+  for (const item of publishables) {
+    const {name, version} = item.manifestContent
+    const tag = `${name}-v${version}`
+    console.info(`$ git tag ${tag}`)
+
+    const {error, signal, status} = childProcess.spawnSync(
+      'git',
+      ['tag', tag],
+      {
+        cwd: projdir,
+        stdio: 'inherit'
+      }
+    )
+
+    if (error) throw error
+
+    if (status) {
+      console.error({signal, status})
+      finalStatus |= status
+    }
+  }
+
+  return finalStatus
+}
