41039647333ccc16edb2f700e64271ea83444288
khai96_ <hvksmr1996@gmail.com>
2018-05-13 09:11:39 +0700

    Remove top-level describe

--- packages/typescript/nested-workspace-helper/test/lib.test/outdated.test/index.test.ts
+++ packages/typescript/nested-workspace-helper/test/lib.test/outdated.test/index.test.ts
@@ -3,42 +3,40 @@ import {listOutdatedDependencies} from '../../../index'
 
 const {apply} = createSetupTeardown('outdated.yaml')
 
-describe('listOutdatedDependencies', () => {
-  it('without options', apply(async () => {
-    const outdated = await listOutdatedDependencies('root')
+it('without options', apply(async () => {
+  const outdated = await listOutdatedDependencies('root')
+  expect(outdated).toMatchSnapshot()
+}))
+
+describe('with options', () => {
+  it('with beOnInstalled=true', apply(async () => {
+    const outdated = await listOutdatedDependencies('root', {baseOnInstalled: true})
     expect(outdated).toMatchSnapshot()
   }))
 
-  describe('with options', () => {
-    it('with beOnInstalled=true', apply(async () => {
-      const outdated = await listOutdatedDependencies('root', {baseOnInstalled: true})
-      expect(outdated).toMatchSnapshot()
-    }))
+  it('with custom generateVersionRequirement', apply(async () => {
+    const outdated = await listOutdatedDependencies('root', {
+      generateVersionRequirement: x => x
+    })
 
-    it('with custom generateVersionRequirement', apply(async () => {
-      const outdated = await listOutdatedDependencies('root', {
-        generateVersionRequirement: x => x
-      })
+    expect(outdated).toMatchSnapshot()
+  }))
 
-      expect(outdated).toMatchSnapshot()
-    }))
+  it('with custom transformVersionRequirement', apply(async () => {
+    const outdated = await listOutdatedDependencies('root', {
+      transformVersionRequirement: x => x
+    })
 
-    it('with custom transformVersionRequirement', apply(async () => {
-      const outdated = await listOutdatedDependencies('root', {
-        transformVersionRequirement: x => x
-      })
+    expect(outdated).toMatchSnapshot()
+  }))
 
-      expect(outdated).toMatchSnapshot()
-    }))
+  it('with all custom options', apply(async () => {
+    const outdated = await listOutdatedDependencies('root', {
+      baseOnInstalled: true,
+      generateVersionRequirement: x => x,
+      transformVersionRequirement: x => x
+    })
 
-    it('with all custom options', apply(async () => {
-      const outdated = await listOutdatedDependencies('root', {
-        baseOnInstalled: true,
-        generateVersionRequirement: x => x,
-        transformVersionRequirement: x => x
-      })
-
-      expect(outdated).toMatchSnapshot()
-    }))
-  })
+    expect(outdated).toMatchSnapshot()
+  }))
 })
