738ba8fa55679301456f25c14bfe994901f96a3a
khai96_ <hvksmr1996@gmail.com>
2018-05-21 16:53:54 +0700

    Fix: codecov not found

--- sh/ci/codecov.sh
+++ sh/ci/codecov.sh
@@ -1,7 +1,7 @@
 echo 'COVERAGE SERVICE: Codecov <https://codecov.io/>'
 
 echo 'Installing codecov...'
-pnpm install --global --shamefully-flatten coveralls || exit $?
+pnpm install --global --shamefully-flatten codecov || exit $?
 
 echo 'Reporting coverage info...'
 codecov
