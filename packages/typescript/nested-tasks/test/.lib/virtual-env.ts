b6f17984130ae4d0b33dc69c5c7e886036f29336
khai96_ <hvksmr1996@gmail.com>
2018-06-02 13:42:47 +0700

    Clone tsconfig

--- packages/typescript/nested-tasks/test/.lib/virtual-env.ts
+++ packages/typescript/nested-tasks/test/.lib/virtual-env.ts
@@ -7,7 +7,8 @@ const treeDir = path.resolve(__dirname, '../.data/tree')
 
 const tree = {
   api: new Symlink(path.resolve(treeDir, 'api')),
-  root: new Clone(path.resolve(treeDir, 'root'))
+  root: new Clone(path.resolve(treeDir, 'root')),
+  'tsconfig.json': new Clone(path.resolve(__dirname, '../../../tsconfig.json5'))
 }
 
 export const createVirtualEnvironment = () =>
