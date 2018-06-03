ca30fa1bccda00dbfdda0da7d43501c2b9653a2b
khai96_ <hvksmr1996@gmail.com>
2018-06-03 14:16:52 +0700

    Complete a test suite

---
+++ packages/typescript/nested-tasks/test/.data/tree/root/custom-deep-func/script-only/task.ts
@@ -0,0 +1,1 @@
+export * from '../../../api' // must have something to not cause error
--- packages/typescript/nested-tasks/test/list-manifest-files.test/index.test.ts
+++ packages/typescript/nested-tasks/test/list-manifest-files.test/index.test.ts
@@ -1,12 +1,85 @@
 import {listManifestFiles} from '../../index'
 import createVirtualEnvironment from '../.lib/virtual-env'
+import createFileChooser from '../../lib/.utils/file-chooser'
+import FileChooser = createFileChooser.FileChooser
+import DeepFunc = listManifestFiles.DeepFunc
+import DEFAULT_FILE_CHOOSER = listManifestFiles.DEFAULT_FILE_CHOOSER
+import DEFAULT_DEEP_FUNCTION = listManifestFiles.DEFAULT_DEEP_FUNCTION
 
-const {apply} = createVirtualEnvironment(true)
+const chooseTypeScript =
+  (param: FileChooser.Param): FileChooser.Result =>
+    param.item === 'task.ts' ? 'module' : DEFAULT_FILE_CHOOSER(param)
 
-it('without options', apply(async () => {
-  const sample = await getSample('root')
-  expect(sample).toMatchSnapshot()
-}))
+const ignoreCustomDeepFunc: DeepFunc = param =>
+  param.item !== 'custom-deep-func' && DEFAULT_DEEP_FUNCTION(param)
+
+describe('without javascript files', () => {
+  const {apply} = createVirtualEnvironment()
+
+  it('without options', apply(async () => {
+    const sample = await getSample('root')
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom deep function', apply(async () => {
+    const sample = await getSample('root', {
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser and deep function', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript,
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+})
+
+describe('with javascript files', () => {
+  const {apply} = createVirtualEnvironment(true)
+
+  it('without options', apply(async () => {
+    const sample = await getSample('root')
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom deep function', apply(async () => {
+    const sample = await getSample('root', {
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser and deep function', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript,
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+})
 
 async function getSample (dirname: string, options?: listManifestFiles.Options) {
   const sample = await listManifestFiles(dirname, options)
