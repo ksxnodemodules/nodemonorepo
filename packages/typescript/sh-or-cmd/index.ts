72e0d89ff02ecf7341301e130cc1f12ce372bb29
khai96_ <hvksmr1996@gmail.com>
2018-06-07 18:02:05 +0700

    Move to typescript folder

---
+++ packages/typescript/sh-or-cmd/index.ts
@@ -0,0 +1,32 @@
+import * as os from 'os'
+
+export type Platform = NodeJS.Platform
+export const platform = os.platform()
+
+export function get<Win32 extends string, POSIX extends string>(
+  options: get.Options<Win32, POSIX>
+): get.Result<Win32, POSIX> {
+  return platform === 'win32' ? options.win32 : options.posix
+}
+
+export namespace get {
+  export interface Options<Win32 extends string, POSIX extends string> {
+    readonly win32: Win32
+    readonly posix: POSIX
+  }
+
+  export type Result<Win32 extends string, POSIX extends string> = Win32 | POSIX
+
+  export namespace Default {
+    export type Win32 = 'cmd'
+    export type POSIX = 'sh'
+    export type Options = get.Options<Win32, POSIX>
+    export type Result = get.Result<Win32, POSIX>
+    export const win32: Win32 = 'cmd'
+    export const posix: POSIX = 'sh'
+    export const options: Options = {win32, posix}
+    export const result: Result = get(options)
+  }
+}
+
+export default get.Default.result
