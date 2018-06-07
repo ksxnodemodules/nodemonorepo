673b17dd2025edc9af75ce8387eee650cb1f55d5
khai96_ <hvksmr1996@gmail.com>
2018-06-07 16:36:36 +0700

    Complete test for loadManifestFiles

--- packages/typescript/nested-tasks/test/load-manifest-files/index.test.ts
+++ packages/typescript/nested-tasks/test/load-manifest-files/index.test.ts
@@ -7,3 +7,76 @@ import {
   chooseTypeScript,
   ignoreCustomDeepFunc
 } from '../.lib/utils'
+
+describe('without javascript files', () => {
+  const {apply} = createVirtualEnvironment()
+
+  it('without options', apply(async () => {
+    const sample = await getSample('root')
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom deep function', apply(async () => {
+    const sample = await getSample('root', {
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser and deep function', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript,
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+})
+
+describe('with javascript files', () => {
+  const {apply} = createVirtualEnvironment(true)
+
+  it('without options', apply(async () => {
+    const sample = await getSample('root')
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom deep function', apply(async () => {
+    const sample = await getSample('root', {
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+
+  it('with custom chooser and deep function', apply(async () => {
+    const sample = await getSample('root', {
+      choose: chooseTypeScript,
+      deep: ignoreCustomDeepFunc
+    })
+
+    expect(sample).toMatchSnapshot()
+  }))
+})
+
+async function getSample (dirname: string, options?: loadManifestFiles.Options) {
+  const sample = await loadManifestFiles(dirname, options)
+  return sample.map(({descriptor: {path}, tasks}) => ({path, tasks}))
+}
