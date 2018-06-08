735b4ec218b82188081e8d193482f377327d9318
khai96_ <hvksmr1996@gmail.com>
2018-06-08 09:37:00 +0700

    Simplify mocks

--- packages/typescript/nested-tasks/__mocks__/sh-or-cmd/index.ts
+++ packages/typescript/nested-tasks/__mocks__/sh-or-cmd/index.ts
@@ -1,18 +1,3 @@
-import * as proto from 'sh-or-cmd'
-
-const out: typeof proto = {
-  default: 'sh',
-  get: Object.assign(
-    (_: any, posix: string) => posix,
-    {
-      ...proto.get,
-      Default: {
-        ...proto.get.Default,
-        result: 'sh'
-      }
-    } as typeof proto.get
-  ),
-  platform: 'linux'
-}
-
-export = out
+export const get = (_: any, posix: string) => posix
+export const platform = 'linux'
+export default 'sh'
