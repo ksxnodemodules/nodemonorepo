484625ea0a52ce481d769b56b0d8b9a9e0e239c4
khai96_ <hvksmr1996@gmail.com>
2018-06-09 12:19:22 +0700

    Update

--- packages/typescript/nested-tasks/lib/execute/index.ts
+++ packages/typescript/nested-tasks/lib/execute/index.ts
@@ -1,10 +1,30 @@
 import {Task, TaskSet} from '../.types'
 import loadManifestFiles from '../load-manifest-files'
 
-export async function execute (dirname: string) {
-
+/**
+ *
+ * @param dirname Directory that contains all task file
+ * @param taskpath Dot separated task names
+ * @param options Options to pass to `loadManifestFiles`
+ */
+export async function execute (
+  dirname: string,
+  taskpath: string,
+  options?: execute.Options
+) {
+  return execute.fromList(
+    await loadManifestFiles(dirname, options),
+    taskpath.split('.')
+  )
 }
 
-export namespace execute {}
+export namespace execute {
+  export async function fromList (
+    tasklist: ReadonlyArray<loadManifestFiles.Result.Item>,
+    taskpath: ReadonlyArray<string>
+  ) {}
+
+  export type Options = loadManifestFiles.Options
+}
 
 export default execute
