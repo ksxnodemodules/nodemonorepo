ec9ed3d09a43d416e62fc64d8bad6c62ca8c3b7d
khai96_ <hvksmr1996@gmail.com>
2018-06-09 12:22:41 +0700

    Add description

--- packages/typescript/nested-tasks/lib/execute/index.ts
+++ packages/typescript/nested-tasks/lib/execute/index.ts
@@ -2,7 +2,7 @@ import {Task, TaskSet} from '../.types'
 import loadManifestFiles from '../load-manifest-files'
 
 /**
- *
+ * Execute all tasks that match `taskpath` and found winthin directory of `dirname`
  * @param dirname Directory that contains all task file
  * @param taskpath Dot separated task names
  * @param options Options to pass to `loadManifestFiles`
@@ -20,7 +20,7 @@ export async function execute (
 
 export namespace execute {
   /**
-   *
+   * Execute all tasks that match `taskpath` within `tasklist`
    * @param tasklist Returning value of `loadManifestFiles`
    * @param taskpath Array of task's/taskset's names
    */
