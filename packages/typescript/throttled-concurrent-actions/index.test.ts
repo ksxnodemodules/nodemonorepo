9f824bec08924a160db8e135926f68aafe3fe429
khai96_ <hvksmr1996@gmail.com>
2018-06-24 12:03:39 +0700

    Rename to past

--- packages/typescript/throttled-concurrent-actions/index.test.ts
+++ packages/typescript/throttled-concurrent-actions/index.test.ts
@@ -4,7 +4,7 @@ import subject from './index'
 it('matches snapshot', async () => {
   const actions = ramda
     .range(0, 32)
-    .map(value => ({value: prev}: {value?: number} = {}) => ({value, prev}))
+    .map(value => ({value: past}: {value?: number} = {}) => ({value, past}))
     .map((fn, i) => i & 1 ? fn : async <X>(x: X) => fn(x))
 
   const promise = subject(5, actions)
