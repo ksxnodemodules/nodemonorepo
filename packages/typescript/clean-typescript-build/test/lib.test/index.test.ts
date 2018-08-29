5d1514332db79a5378260133eef28b74caae4c1e
khai96_ <hvksmr1996@gmail.com>
2018-08-29 17:54:52 +0700

    Complete first test

--- packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
+++ packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
@@ -1,12 +1,35 @@
-// import * as fsTreeUtils from 'fs-tree-utils'
+import * as fsTreeUtils from 'fs-tree-utils'
 import {clean, listAllTargets} from '../../index'
 import createVirtualEnv from '../.lib/virtual-env'
 
 const {apply} = createVirtualEnv()
 
 it('should list correct target files', apply(async () => {
-  const targets = await listAllTargets('root')
-  expect(targets).toMatchSnapshot()
-  const invalids = targets.filter(x => !/(\.jsx?|\.d\.tsx?)(\.map)?$/.test(x))
-  expect(invalids).toEqual([])
+  const [all, targets] = await Promise.all([
+    fsTreeUtils.read.flat('root'),
+    listAllTargets('root')
+  ])
+
+  const deletedFileContents = targets.map(filename => ({
+    filename,
+    content: all.fileContents[filename]
+  }))
+
+  const remainingFileContents = all.files
+    .filter(filename => !targets.includes(filename) && !/__describe$/.test(filename))
+    .map(filename => ({filename, content: all.fileContents[filename]}))
+
+  expect({
+    targets,
+    deletedFileContents,
+    remainingFileContents
+  }).toMatchSnapshot()
+
+  expect({
+    invalidAcceptance: remainingFileContents.filter(x => x.content !== 'Should be kept'),
+    invalidDeletion: deletedFileContents.filter(x => x.content !== 'Should be deleted')
+  }).toEqual({
+    invalidAcceptance: [],
+    invalidDeletion: []
+  })
 }))
