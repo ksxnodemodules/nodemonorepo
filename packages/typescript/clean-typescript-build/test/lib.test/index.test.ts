5b1e8763474a6e3e579947068a1cf970baec4953
khai96_ <hvksmr1996@gmail.com>
2018-08-29 19:34:08 +0700

    Complete index.test.ts

--- packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
+++ packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
@@ -1,4 +1,5 @@
 import * as fsTreeUtils from 'fs-tree-utils'
+import {FlatReadResultValue} from 'fs-tree-utils/lib/read'
 import {clean, listAllTargets} from '../../index'
 import createVirtualEnv from '../.lib/virtual-env'
 
@@ -10,13 +11,36 @@ it('should list correct target files', apply(async () => {
     listAllTargets('root')
   ])
 
+  testTargetCorrectness(all, targets)
+}))
+
+it('should delete correct target files', apply(async () => {
+  const before = await fsTreeUtils.read.flat('root')
+  const report = await clean('root')
+  const after = await fsTreeUtils.read.flat('root')
+
+  testTargetCorrectness(before, report.targets)
+
+  expect(report.success).toEqual(report.targets)
+  expect(report.failure).toEqual([])
+
+  expect({
+    report,
+    state: [
+      {before},
+      {after}
+    ]
+  }).toMatchSnapshot()
+}))
+
+function testTargetCorrectness (all: FlatReadResultValue, targets: ReadonlyArray<string>) {
   const deletedFileContents = targets.map(filename => ({
     filename,
     content: all.fileContents[filename]
   }))
 
   const remainingFileContents = all.files
-    .filter(filename => !targets.includes(filename) && !/__describe$/.test(filename))
+    .filter(filename => !targets.includes(filename) && !/(^|\/)__[a-zA-Z]+$/.test(filename))
     .map(filename => ({filename, content: all.fileContents[filename]}))
 
   expect({
@@ -32,4 +56,4 @@ it('should list correct target files', apply(async () => {
     invalidAcceptance: [],
     invalidDeletion: []
   })
-}))
+}
