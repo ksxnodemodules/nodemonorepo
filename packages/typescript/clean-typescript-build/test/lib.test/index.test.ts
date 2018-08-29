4fe4e1c8e86aaaf21b367ac6ee34505496743d08
khai96_ <hvksmr1996@gmail.com>
2018-08-29 17:22:50 +0700

    Add a test

--- packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
+++ packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
@@ -1,1 +1,12 @@
+// import * as fsTreeUtils from 'fs-tree-utils'
 import {clean, listAllTargets} from '../../index'
+import createVirtualEnv from '../.lib/virtual-env'
+
+const {apply} = createVirtualEnv()
+
+it('should list correct target files', apply(async () => {
+  const targets = await listAllTargets('root')
+  expect(targets).toMatchSnapshot()
+  const invalids = targets.filter(x => !/(\.jsx?|\.d\.tsx?)(\.map)?$/.test(x))
+  expect(invalids).toEqual([])
+}))
