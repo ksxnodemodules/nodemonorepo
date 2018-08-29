431a08a8971066a3f3425b4842993d35b7d7204f
khai96_ <hvksmr1996@gmail.com>
2018-08-30 01:49:37 +0700

    Move testTargetCorrectness to its own directory

---
+++ packages/typescript/clean-typescript-build/test/.lib/target-correctness.ts
@@ -0,0 +1,28 @@
+import {FlatReadResultValue} from 'fs-tree-utils/lib/read'
+
+export function testTargetCorrectness (all: FlatReadResultValue, targets: ReadonlyArray<string>) {
+  const deletedFileContents = targets.map(filename => ({
+    filename,
+    content: all.fileContents[filename]
+  }))
+
+  const remainingFileContents = all.files
+    .filter(filename => !targets.includes(filename) && !/(^|\/)__[a-zA-Z]+$/.test(filename))
+    .map(filename => ({filename, content: all.fileContents[filename]}))
+
+  expect({
+    targets,
+    deletedFileContents,
+    remainingFileContents
+  }).toMatchSnapshot()
+
+  expect({
+    invalidAcceptance: remainingFileContents.filter(x => x.content !== 'Should be kept'),
+    invalidDeletion: deletedFileContents.filter(x => x.content !== 'Should be deleted')
+  }).toEqual({
+    invalidAcceptance: [],
+    invalidDeletion: []
+  })
+}
+
+export default testTargetCorrectness
--- packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
+++ packages/typescript/clean-typescript-build/test/lib.test/index.test.ts
@@ -1,7 +1,7 @@
 import * as fsTreeUtils from 'fs-tree-utils'
-import {FlatReadResultValue} from 'fs-tree-utils/lib/read'
 import {clean, listAllTargets} from '../../index'
 import createVirtualEnv from '../.lib/virtual-env'
+import testTargetCorrectness from '../.lib/target-correctness'
 
 const {apply} = createVirtualEnv()
 
@@ -32,28 +32,3 @@ it('should delete correct target files', apply(async () => {
     ]
   }).toMatchSnapshot()
 }))
-
-function testTargetCorrectness (all: FlatReadResultValue, targets: ReadonlyArray<string>) {
-  const deletedFileContents = targets.map(filename => ({
-    filename,
-    content: all.fileContents[filename]
-  }))
-
-  const remainingFileContents = all.files
-    .filter(filename => !targets.includes(filename) && !/(^|\/)__[a-zA-Z]+$/.test(filename))
-    .map(filename => ({filename, content: all.fileContents[filename]}))
-
-  expect({
-    targets,
-    deletedFileContents,
-    remainingFileContents
-  }).toMatchSnapshot()
-
-  expect({
-    invalidAcceptance: remainingFileContents.filter(x => x.content !== 'Should be kept'),
-    invalidDeletion: deletedFileContents.filter(x => x.content !== 'Should be deleted')
-  }).toEqual({
-    invalidAcceptance: [],
-    invalidDeletion: []
-  })
-}
