02f711eeea9793a741a0975696e0e9ec2033af6c
khai96_ <hvksmr1996@gmail.com>
2018-10-01 01:02:49 +0700

    No more needs for manually restore mocks

--- packages/typescript/exec-inline/test/index.test.ts
+++ packages/typescript/exec-inline/test/index.test.ts
@@ -5,14 +5,14 @@ import spawnSync, { SpawnSyncRepresented } from '../index'
 it('matches snapshot', () => {
   const info: { [_: string]: any } = {}
 
-  const spySpawnSync = jest
+  jest
     .spyOn(childProcess, 'spawnSync')
     .mockImplementation((...args: any[]): SpawnSyncRepresented => {
       info.spawnSyncArgs = args
       return { status: 123 }
     })
 
-  const spyProcessExit = jest
+  jest
     .spyOn(process, 'exit')
     .mockImplementation((...args: any[]) => {
       info.processExitArgs = args
@@ -21,14 +21,6 @@ it('matches snapshot', () => {
   const representative = spawnSync('arg0', 'arg1', 'arg2', 'arg3')
   info.representative = representative
   representative.exit()
-
-  // tslint:disable-next-line:comment-format
-  //@ts-ignore
-  spySpawnSync.mockRestore()
-
-  // tslint:disable-next-line:comment-format
-  //@ts-ignore
-  spyProcessExit.mockRestore()
 
   expect(info).toMatchSnapshot()
 })
