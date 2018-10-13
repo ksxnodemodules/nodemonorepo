6ade030c2691213ed2a0614fd3abfc825d3f70e3
khai96_ <hvksmr1996@gmail.com>
2018-10-13 13:53:18 +0700

    Move private getEntry to public find

--- packages/typescript/advanced-map-altered-equal/index.ts
+++ packages/typescript/advanced-map-altered-equal/index.ts
@@ -29,7 +29,7 @@ class AlteredEqual<
     this.equal = equal
   }
 
-  private getEntry (key: Key): FindingResult<Readonly<{ key: Key, value: Value }>> {
+  public find (key: Key): FindingResult<Readonly<{ key: Key, value: Value }>> {
     const { equal } = this
 
     for (const [k, v] of this.data) {
@@ -50,21 +50,21 @@ class AlteredEqual<
   }
 
   public has (key: Key): boolean {
-    return this.getEntry(key).found
+    return this.find(key).found
   }
 
   public get (key: Key): Value | undefined {
-    const res = this.getEntry(key)
+    const res = this.find(key)
     return res.found ? res.value.value : undefined
   }
 
   public set (key: Key, value: Value) {
-    const res = this.getEntry(key)
+    const res = this.find(key)
     this.data.set(res.found ? res.value.key : key, value)
   }
 
   public delete (key: Key) {
-    const res = this.getEntry(key)
+    const res = this.find(key)
 
     if (res.found) {
       this.data.delete(res.value.key)
