a225049233e01abf18fcf2a9dc6ae68567a07cf4
khai96_ <hvksmr1996@gmail.com>
2018-10-19 21:04:16 +0700

    Create reusable spy() function

--- packages/typescript/exec-inline/test/index.test.ts
+++ packages/typescript/exec-inline/test/index.test.ts
@@ -2,28 +2,52 @@ import childProcess from 'child_process'
 import process from 'process'
 import spawnSync, { SpawnSyncRepresented } from '../index'
 
-it('matches snapshot', () => {
-  const info: { [_: string]: any } = {}
-
+const spy = ({
+  spawnSync,
+  exit
+}: {
+  spawnSync: (cmd: string, args: string[], options: {}) => any,
+  exit: (status: number) => void
+}) => {
   const spySpawnSync = jest
     .spyOn(childProcess, 'spawnSync')
-    .mockImplementation((...args: any[]): SpawnSyncRepresented => {
-      info.spawnSyncArgs = args
-      return { status: 123 }
-    })
+    .mockImplementation(spawnSync)
 
   const spyProcessExit = jest
     .spyOn(process, 'exit')
-    .mockImplementation((...args: any[]) => {
+    .mockImplementation(exit)
+
+  const restore = () => {
+    spySpawnSync.mockRestore()
+    spyProcessExit.mockRestore()
+  }
+
+  return {
+    spySpawnSync,
+    spyProcessExit,
+    restore
+  }
+}
+
+it('matches snapshot', () => {
+  const info: { [_: string]: any } = {}
+
+  const spyObject = spy({
+    spawnSync: (...args: any[]) => {
+      info.spawnSyncArgs = args
+      return { status: 123 }
+    },
+
+    exit: (...args: any[]) => {
       info.processExitArgs = args
-    })
+    }
+  })
 
   const representative = spawnSync('arg0', 'arg1', 'arg2', 'arg3')
   info.representative = representative
   representative.exit()
 
-  spySpawnSync.mockRestore()
-  spyProcessExit.mockRestore()
+  spyObject.restore()
 
   expect(info).toMatchSnapshot()
 })
