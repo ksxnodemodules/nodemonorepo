7f0b59adfa6f89ac35f211c21d42b7ce2173ccfd
khai96_ <hvksmr1996@gmail.com>
2018-11-09 16:12:31 +0700

    Add doccomments

--- packages/typescript/cached-factorial/index.ts
+++ packages/typescript/cached-factorial/index.ts
@@ -1,5 +1,18 @@
 import { AdjadentNumberedCalculator } from 'cached-expression-shared-utils'
 
+/**
+ * Utilize [cached-expression](https://www.npmjs.com/package/cached-expression)
+ * to calculate multiple factorial numbers
+ *
+ * **Definition:**
+ * * 0! = 1! = 1
+ * * ∀ n ≥ 2: n! = n * (n - 1)!
+ *
+ * @example
+ * import CachedFactorial from 'cached-factorial'
+ * const { calculate } = new CachedFactorial()
+ * const result = [0, 1, 2, 3, 4, 5].map(calculate) // expect: [1, 1, 2, 6, 24, 120]
+ */
 class CachedFactorial extends AdjadentNumberedCalculator<number> {
   constructor () {
     super(
--- packages/typescript/cached-fibonacci/index.ts
+++ packages/typescript/cached-fibonacci/index.ts
@@ -1,5 +1,19 @@
 import { AdjadentNumberedCalculator } from 'cached-expression-shared-utils'
 
+/**
+ * Utilize [cached-expression](https://www.npmjs.com/package/cached-expression)
+ * to calculate fibonacci numbers
+ *
+ * **Definition:**
+ * * fib(0) = 0
+ * * fib(1) = 1
+ * * ∀ n ≥ 2: fib(n) = fib(n - 2) + fib(n - 1)
+ *
+ * @example
+ * import CachedFibonacci from 'cached-fibonacci'
+ * const { calculate } = new CachedFibonacci()
+ * const result = [0, 1, 2, 3, 4, 5].map(calculate) // expect: [0, 1, 1, 2, 3, 5]
+ */
 class CachedFibonacci extends AdjadentNumberedCalculator<number> {
   constructor () {
     super(x => {
