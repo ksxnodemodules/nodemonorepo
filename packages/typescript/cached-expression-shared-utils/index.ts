0a5a35df0e0ff819bb03df3fc8d3b0b273d955e0
khai96_ <hvksmr1996@gmail.com>
2018-11-09 14:23:18 +0700

    Create cached-expression-shared-utils

---
+++ packages/typescript/cached-expression-shared-utils/index.ts
@@ -0,0 +1,34 @@
+import { MapLike, Calculator } from 'cached-expression'
+
+/**
+ * @private
+ * This class is for `AdjadentNumberedCalculator` only
+ */
+class AdjadentNumberedMap<Value> extends Map<number, Value> implements MapLike<number, Value> {
+  private maxKey = 0
+
+  public get (key: number): Value | undefined {
+    return this.maxKey < key
+      ? undefined
+      : super.get(key)
+  }
+
+  public set (key: number, value: Value): this {
+    if (this.maxKey < key) {
+      this.maxKey = key
+    }
+
+    super.set(key, value)
+
+    return this
+  }
+}
+
+/**
+ * This class is useful when `calculate(n)` requires `calculate(n - 1)` (e.g. factorial, fibonacci)
+ */
+export class AdjadentNumberedCalculator<Value> extends Calculator<number, Value> {
+  protected createCache (): MapLike<number, Value> {
+    return new AdjadentNumberedMap<Value>()
+  }
+}
