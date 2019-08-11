e7dd93f54e5d25256c382fddecc73f18b6c883bf
khai96_ <hvksmr1996@gmail.com>
2019-08-11 12:51:50 +0700

    Add workaround

--- packages/typescript/parse-dependency-range/lib/index.ts
+++ packages/typescript/parse-dependency-range/lib/index.ts
@@ -1,3 +1,17 @@
 export * from './types'
 export * from './constants'
 export * from './functions'
+
+// workaround a stupid bug during test:
+//   TypeError: Cannot read property 'Semver' of undefined
+//
+//     4 |
+//     5 | const samples: ReadonlyArray<Sample> = [
+//   > 6 |   [Type.Semver, [
+//       |         ^
+//     7 |     '0.1.2',
+//     8 |     '=0.1.2',
+//     9 |     '~0.1.2',
+//
+//   at Object.<anonymous> (packages/typescript/parse-dependency-range/test/lib.test/functions.test/parse.test/index.test.ts:6:9)
+export { Type } from './types'
