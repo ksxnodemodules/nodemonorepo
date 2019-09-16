1527f2b9147ba832cb5aa2b66e87006f75a6ed8f
khai96_ <hvksmr1996@gmail.com>
2019-09-16 12:14:20 +0700

    Use repo's own clean-typescript-build to clean typescript output files

--- packages/javascript/clean-typescript-build.private/bin.js
+++ packages/javascript/clean-typescript-build.private/bin.js
@@ -1,2 +1,16 @@
 #! /usr/bin/env preloaded-node
-require('./index')
+const path = require('path')
+const process = require('process')
+const { spawnSync } = require('preloaded-node.private')
+const bin = require.resolve('clean-typescript-build/bin/clean-typescript-build')
+const directory = path.resolve(__dirname, '../../typescript')
+
+const { status } = spawnSync(
+  [bin, directory, ...process.argv.slice(2)],
+  {
+    stdio: 'inherit',
+    shell: true
+  }
+)
+
+process.exit(status)
