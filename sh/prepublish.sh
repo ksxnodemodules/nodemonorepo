00ddb1b50699d42e9106bb96ef8cf156033ef8bf
khai96_ <hvksmr1996@gmail.com>
2019-10-05 10:03:08 +0700

    Add clean-typescript-build to prepublish

--- sh/prepublish.sh
+++ sh/prepublish.sh
@@ -1,4 +1,5 @@
 bash sh/create-npmignore-files.sh || exit $?
 bash sh/mismatched-versions.sh || exit $?
+preloaded-node sh/clean-typescript-build.js || exit $?
 bash sh/test.sh --ci || exit $?
 bash sh/build.sh || exit $?
